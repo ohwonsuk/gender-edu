66ca0d4e601fcc1de6999484f31b17151f622467
ohwonsuk <wsoh51@gmail.com>
2024-02-04 21:39:02 +0900

    heading 색상변경

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Heading = () => {
   return (
     <>
-      <div className="w-auto h-40 mx-auto bg-gradient-to-b from-teal-700 to-teal-50 border-0 rounded">
+      <div className="w-auto h-40 mx-auto bg-gradient-to-b from-teal-600 to-teal-50 border-0 rounded">
         <div className="font-ibmsans text-xs text-pink-900 pt-28 pl-10 font-semibold">
           Gender Edu
         </div>
