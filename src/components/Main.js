6fd94942ab35c1380c4c085d4354c70993578aa0
ohwonsuk <wsoh51@gmail.com>
2024-02-04 22:15:35 +0900

    home 페이지 폰트 및 줄간격조정

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import Intro from './Intro';
 const Main = () => {
   return (
     <>
-      <h1 className="pl-2 mt-5 font-ibmsans text-2xl font-bold content-center">
+      <h1 className="pl-2 mt-5 mb-8 font-ibmsans text-xl font-bold content-center">
         젠더에듀상담센터
       </h1>
       {/* <h4 className="pl-2 font-bold text-xl mb-2">
