33f5926e962c157c8046097f418f0db48078009d
ohwonsuk <wsoh51@gmail.com>
2024-02-17 21:46:50 +0900

    sitemap 추가하기: url 수정등록

--- sitemap.js
+++ sitemap.js
@@ -2,13 +2,9 @@ import Generator from 'react-router-sitemap-generator';
 import Router from './src/components/Router';
 // import Router from './components/Router'; //import your react router component
 
-const generator = new Generator(
-  'https://react-router-sitemap-generator.com',
-  Router(),
-  {
-    lastmod: new Date().toISOString().slice(0, 10),
-    changefreq: 'monthly',
-    priority: 0.8,
-  },
-);
+const generator = new Generator('https://gender-edu.netlify.app', Router(), {
+  lastmod: new Date().toISOString().slice(0, 10),
+  changefreq: 'monthly',
+  priority: 0.8,
+});
 generator.save('public/sitemap.xml');
