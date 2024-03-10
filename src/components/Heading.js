d7244f4a7e448847d146875f5b08d85a08e1a72c
ohwonsuk <wsoh51@gmail.com>
2024-03-10 23:11:51 +0900

    heading 폰트사이즈 키우고, 프로파일 수정

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -4,10 +4,10 @@ const Heading = () => {
   return (
     <>
       <div className="w-[620px] md:w-full h-40 mx-auto bg-gradient-to-b from-teal-500 to-teal-50 border-0 rounded">
-        <div className="text-base text-pink-900 pt-24 pl-10 font-semibold">
+        <div className="text-large text-pink-900 pt-24 pl-10 font-bold">
           Gender Edu
         </div>
-        <div className="text-base text-pink-900 pl-10 font-semibold">
+        <div className="text-large text-pink-900 pl-10 font-bold">
           Counseling Center
         </div>
       </div>
--- src/components/MainProfile.js
+++ src/components/MainProfile.js
@@ -8,7 +8,7 @@ const MainProfile = () => {
       <div className="font-ibmsans pl-2 text-lg mb-2">대표 이 승 은</div>
       <ul className="font-ibmsans pl-6 list-disc leading-relaxed mb-5">
         <li>여성가족부 우수교육사례 장관상 수상 2019</li>
-        <li>경기북부 학교 및 장애아동청소년 성 인권교육</li>
+        {/* <li>경기북부 학교 및 장애아동청소년 성 인권교육</li> */}
         <li>전 한국장애인폭력상담소 소장</li>
         <li>전 서울특별시북부교육지원청 인권지원단 운영위원</li>
         <li>전 한국학교상담복지센터 운영위원</li>
