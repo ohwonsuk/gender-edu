db64dd9bb7a7d776ff8768eccd52986460cd51e9
ohwonsuk <wsoh51@gmail.com>
2024-03-10 23:03:07 +0900

    heading 폰트 및 사이즈 변경

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -4,10 +4,10 @@ const Heading = () => {
   return (
     <>
       <div className="w-[620px] md:w-full h-40 mx-auto bg-gradient-to-b from-teal-500 to-teal-50 border-0 rounded">
-        <div className="font-ibmsans text-xs text-pink-900 pt-28 pl-10 font-semibold">
+        <div className="text-base text-pink-900 pt-24 pl-10 font-semibold">
           Gender Edu
         </div>
-        <div className="font-ibmsans text-xs text-pink-900 pl-10 font-semibold">
+        <div className="text-base text-pink-900 pl-10 font-semibold">
           Counseling Center
         </div>
       </div>
